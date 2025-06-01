6f375ea758fefdfa91129db35d2c067a7b90700d
kirfor <94166912+kirfor@users.noreply.github.com>
2025-06-01 20:25:56 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('textForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const value1 = document.getElementById('input1').value;
-    const value2 = document.getElementById('input2').value;
-    alert(`Проверка:\nПоле 1: ${value1}\nПоле 2: ${value2}`);
-});
