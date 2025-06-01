4b3021c3a9bb51c77972e2a82cfcc380baa8f50e
kirfor <94166912+kirfor@users.noreply.github.com>
2025-06-01 15:08:06 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('textForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const userText = document.getElementById('userInput').value;
-    alert(`Вы ввели: ${userText}`);
-    // Можно также вывести текст на страницу:
-    // document.querySelector('.content').innerHTML += `<p>${userText}</p>`;
-});
