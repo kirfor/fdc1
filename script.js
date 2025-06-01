aa1fe45287138422df21f929291c1b1e940a4026
kirfor <94166912+kirfor@users.noreply.github.com>
2025-06-01 13:34:31 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("Сайт загружен!");
-
-// Пример интерактивности
-document.querySelector('h1').addEventListener('click', () => {
-    alert('ДОБРО Добро пожаловать на мой сайт!!!!!!!!!!!!!!!!!!');
-});
